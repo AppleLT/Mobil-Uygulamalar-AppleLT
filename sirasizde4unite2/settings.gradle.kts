a14f9adb8039fec402ba1d79d34d878aec581afc
AppleLT <ebubekir.dyt@gmail.com>
2024-11-25 00:20:28 +0300

    mobil uyg

---
+++ sirasizde4unite2/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sirasizde4unite2"
+include(":app")
+ 
